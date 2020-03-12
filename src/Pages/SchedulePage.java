1086ef37c6b12f1b3d5c5cf4d93edb9cfab08000
ahmed <tashdiq.ahmed@ontechu.net>
2020-03-12 16:25:41 -0400

    On the schedule page, removed the arbitrary upper left box that had read "Date Time." The spacing however is kept, to allow room for the eventual time of day column.

--- src/Pages/SchedulePage.java
+++ src/Pages/SchedulePage.java
@@ -2,35 +2,46 @@ package Pages;
 import Windows.Window;
 import javafx.application.Application;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 public class SchedulePage extends Page {
     public SchedulePage()
     {
-        Text[] days= new Text[6];
-        days[0]=new Text("\n\n\t\t Days\n\n\tTime");
+        Text[] days= new Text[8];
+        days[0]=new Text("\n\t\tDays\n\n\tTime");
         days[1]=new Text("Monday");
         days[2]=new Text("Tuesday");
         days[3]=new Text("Wednesday");
         days[4]=new Text("Thursday");
         days[5]=new Text("Friday");
+        days[6]=new Text("Saturday");
+        days[7]=new Text("Sunday");
         VBox buttons = new VBox();
-        Button[] b =new Button[6];
+        Button[] b =new Button[8];
 
-        for (int i=0; i<6;i++) {
+        for (int i=1; i<8;i++) {
             b[i] = new Button(days[i].getText());
-            b[i].setPrefSize(100,100);
+            b[i].setPrefSize(125,75);
             buttons.getChildren().add(b[i]);
         }
-
-        mainPane.add(b[0],0,0);
+        ColumnConstraints column = new ColumnConstraints();
+        column.setPercentWidth(20);
+        mainPane.getColumnConstraints().addAll(column, column,column,column,column,column,column,column); // each get 50% of width
+        //RowConstraints row = new RowConstraints();
+        //row.setPercentHeight(20);
+        //mainPane.getRowConstraints().addAll(row, row); // each get 50% of width
+        //mainPane.add(b[0],0,0);
         mainPane.add(b[1],1,0);
         mainPane.add(b[2],2,0);
         mainPane.add(b[3],3,0);
         mainPane.add(b[4],4,0);
         mainPane.add(b[5],5,0);
+        mainPane.add(b[6],6,0);
+        mainPane.add(b[7],7,0);
 
     }
     public static void main(String[] args) {
