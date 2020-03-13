05b04c899eaf15cc4b59b61926248776fc940f6d
ArshadKhan-UOIT <arshad.khan@ontariotechu.net>
2020-03-13 11:53:19 -0400

    minor changes to 'csv' and args in 'Midterm', 'Exam', and 'Courses'

--- src/DataStructures/Exam.java
+++ src/DataStructures/Exam.java
@@ -2,14 +2,16 @@ package DataStructures;
 
 public class Exam {
     private String courseCode;
+    private String exam;
     private String date;
     private String time;
     private String location;
     private double weight;
     private double mark;
 
-    public Exam(String s, String s1, String s2, String s3, double parseDouble, double parseDouble1) {
+    public Exam(String s, String exam, String s1, String s2, String s3, double parseDouble, double parseDouble1) {
         courseCode = s;
+        this.exam = exam;
         date = s1;
         time = s2;
         location = s3;
@@ -19,12 +21,19 @@ public class Exam {
 
     public Exam(String[] e) {
         courseCode = e[0];
-        date = e[1];
-        time = e[2];
-        location = e[3];
-        this.weight = Double.parseDouble(e[4]);
-        this.mark = Double.parseDouble(e[5]);
+        exam = e[1];
+        date = e[2];
+        time = e[3];
+        location = e[4];
+        this.weight = Double.parseDouble(e[5]);
+        this.mark = Double.parseDouble(e[6]);
     }
+
+    public String getCourseCode() { return courseCode; }
+    public String getExam() { return exam; }
+    public String getDate() { return date; }
+    public String getTime() { return time; }
+    public String getLocation() { return location; }
 
     public double getMark() {
         return mark;
