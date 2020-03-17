c964246d31a0bee301e5b79a76c5d4abbe87c015
Ryan-7926 <ryan.christopher1@ontariotechu.net>
2020-03-17 12:22:15 -0400

    Updated CoursesPage.java with Arshad's code so it can be merged to master

--- src/Pages/CoursesPage.java
+++ src/Pages/CoursesPage.java
@@ -7,6 +7,7 @@ import DataStructures.Midterm;
 import Windows.Window;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.scene.chart.PieChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -14,6 +15,8 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 
 public class CoursesPage extends Page {
@@ -29,14 +32,21 @@ public class CoursesPage extends Page {
     TableColumn<Assignment, String> asmtCol, asmtDueDateCol;
     TableColumn<Assignment, Double> asmtWightCol, asmtMarkCol;
     String[] courseCodes = {"MATH2050", "CSCI2160", "CSCI2040", "CSCI2072", "CSCI2020"};
+    String[] courseContent = {"Asmt (%)", "Midterm (%)", "Exam (%)"};
+    PieChart pieChart = new PieChart();
+    Text[] text = new Text[]{new Text(), new Text(), new Text()};
     BorderPane bPane = new BorderPane();
     GridPane centerPane = new GridPane();
+    Pane rightPane = new Pane();
 
     public CoursesPage() {
         courseTableView = new TableView();
         examTableView = new TableView();
         midtermTableView = new TableView();
         assignmentTableView = new TableView();
+        courseTableView.setMaxHeight(200);  //267
+        examTableView.setMaxHeight(200);
+        midtermTableView.setMaxHeight(200);
         System.out.println("Pages.CoursesPage created");
         HBox buttons = new HBox();
 
@@ -52,29 +62,123 @@ public class CoursesPage extends Page {
         for (int i = 0; i < 5; i++) {
             final int index = i;
             b[i].setOnMouseClicked(e -> {
-                if (index == 0) {
-                    getTableColumn(index);
-                } else if (index == 1) {
-                    getTableColumn(index);
-                } else if (index == 2) {
-                    getTableColumn(index);
-                } else if (index == 3) {
-                    getTableColumn(index);
-                } else if (index == 4) {
-                    getTableColumn(index);
-                }
+                getTableColumn(index);
+                getPieGraph(index);
             });
+        }
+        for (int i = 0; i < text.length; i++) {
+            text[i].setText(courseContent[i]);
         }
         bPane.setTop(buttons);
         bPane.setCenter(centerPane);
+        bPane.setRight(rightPane);
         mainPane.add(bPane, 0, 0);
+//        mainPane.add(rightPane,2,0);
+
     }
 
+    private void getPieGraph(int index) {
+        //y=635/3 = ~211    x = (1268-702)/2 = 283
+        double xPosition, yPosition;
+//        double[] asmntTotal = new double[5];
+//        double[] midTotal = new double[5];
+//        double[] examTotal = new double[5];
+        double[][] courseWeightArr = new double[5][3]; //[index][content] -> [For each Course][Content for each Course (assignment,midterm,exam)]
+        double[] overAllTotal = new double[5];
+        xPosition = (mainPane.getWidth() - 702) / 3;
+        yPosition = mainPane.getHeight() / 3.0;
+        for (int i = 0; i < overAllTotal.length; i++) {
+//            asmntTotal[i] = 0;
+//            examTotal[i] = 0;
+//            midTotal[i] = 0;
+            courseWeightArr[i][0] = 0; //initializing
+            courseWeightArr[i][1] = 0; //initializing
+            courseWeightArr[i][2] = 0; //initializing
+            overAllTotal[i] = 0;
+        }
+        for (Course c : Window.courses) {
+
+            Assignment[] assignmentList = c.getAssignments();
+
+            for (Assignment a : assignmentList) {
+                if (a.getCourseCode().equals(courseCodes[index])) {
+//                    asmntTotal[index] += a.getWeight();
+                    courseWeightArr[index][0] += a.getWeight();
+                    overAllTotal[index] += a.getWeight();
+                }
+            }
+            Midterm[] midtermList = c.getMidterms();
+
+            for (Midterm m : midtermList) {
+                if (m.getCourseCode().equals(courseCodes[index])) {
+//                    midTotal[index] += m.getWeight();
+                    courseWeightArr[index][1] += m.getWeight();
+                    overAllTotal[index] += m.getWeight();
+                }
+            }
+            Exam[] examList = c.getExam();
+
+            for (Exam e : examList) {
+                if (e.getCourseCode().equals(courseCodes[index])) {
+//                    examTotal[index] += e.getWeight();
+                    courseWeightArr[index][2] += e.getWeight();
+                    overAllTotal[index] += e.getWeight();
+                }
+            }
+        }
+        //        System.out.println( "Index = " + index + "\teTot = " + examTotal[index] + "\tmTot = " + midTotal[index] + "\taTot = " + asmntTotal[index] + "\toTot = " + overAllTotal[index] + "\n");
+//        System.out.println();
+//        System.out.print("A\t");
+//        for (int i=0;i<overAllTotal.length;i++) {
+//            System.out.print(courseWeightArr[i][0] + " \t");
+//        }
+//        System.out.println();
+//        System.out.print("M\t");
+//        for (int i=0;i<overAllTotal.length;i++) {
+//            System.out.print(courseWeightArr[i][1] + " \t");
+//        }
+//        System.out.println();
+//        System.out.print("E\t");
+//        for (int i=0;i<overAllTotal.length;i++) {
+//            System.out.print(courseWeightArr[i][2] + " \t");
+//        }
+//        System.out.println();
+//        output:
+        //A	3.0 	0.0 	0.0 	0.0 	0.0
+        //M	40.0 	0.0 	0.0 	0.0 	0.0
+        //E	40.0 	0.0 	0.0 	0.0 	0.0
+        //
+        //A	0.0 	35.0 	0.0 	0.0 	0.0
+        //M	0.0 	20.0 	0.0 	0.0 	0.0
+        //E	0.0 	20.0 	0.0 	0.0 	0.0
+        //
+        //A	0.0 	0.0 	30.0 	0.0 	0.0
+        //M	0.0 	0.0 	20.0 	0.0 	0.0
+        //E	0.0 	0.0 	40.0 	0.0 	0.0
+        //
+        //A	0.0 	0.0 	0.0 	25.0 	0.0
+        //M	0.0 	0.0 	0.0 	15.0 	0.0
+        //E	0.0 	0.0 	0.0 	50.0 	0.0
+        //
+        //A	0.0 	0.0 	0.0 	0.0 	35.0
+        //M	0.0 	0.0 	0.0 	0.0 	20.0
+        //E	0.0 	0.0 	0.0 	0.0 	25.0
+        Pane rightPane = new Pane();
+        ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
+        for (int contentIndex = 0; contentIndex < courseWeightArr[index].length; contentIndex++) {
+            pieChartData.add(new PieChart.Data(courseContent[contentIndex], courseWeightArr[index][contentIndex]));
+        }
+        pieChart.setData(pieChartData);
+        pieChart.setTitle("Weight's");
+        rightPane.getChildren().add(pieChart);
+        bPane.setRight(rightPane);
+//        mainPane.add(rightPane,2,0);
+    }
     private void getTableColumn(int index) {
         courseCodeCol = new TableColumn("Course Code");
         courseCodeCol.setMinWidth(200);
         courseProfCol = new TableColumn("Prof");
-        courseProfCol.setMinWidth(100);
+        courseProfCol.setMinWidth(200);
         courseDaysCol = new TableColumn("Days");
         courseDaysCol.setMinWidth(100);
         courseTimeCol = new TableColumn("Times");
@@ -164,11 +268,11 @@ public class CoursesPage extends Page {
         asmtCol = new TableColumn("Type");
         asmtCol.setMinWidth(200);
         asmtDueDateCol = new TableColumn("Due Date");
-        asmtDueDateCol.setMinWidth(100);
+        asmtDueDateCol.setMinWidth(200);
         asmtWightCol = new TableColumn("Weight (%)");
-        asmtWightCol.setMinWidth(100);
+        asmtWightCol.setMinWidth(150);
         asmtMarkCol = new TableColumn("Mark (%)");
-        asmtMarkCol.setMinWidth(100);
+        asmtMarkCol.setMinWidth(150);
         asmtCol.setCellValueFactory(new PropertyValueFactory<>("assignmentName"));
         asmtDueDateCol.setCellValueFactory(new PropertyValueFactory<>("dueDate"));
         asmtWightCol.setCellValueFactory(new PropertyValueFactory<>("weight"));
@@ -186,6 +290,7 @@ public class CoursesPage extends Page {
         }
         assignmentTableView.getColumns().setAll(asmtCol, asmtDueDateCol, asmtWightCol, asmtMarkCol);
 
+
         GridPane centerPane = new GridPane();
 
         centerPane.add(courseTableView, 0, 1);
@@ -194,6 +299,7 @@ public class CoursesPage extends Page {
         centerPane.add(assignmentTableView, 0, 4);
 
         bPane.setCenter(centerPane);
+//        System.out.println(courseTableView.getHeight());
 
     }
 
