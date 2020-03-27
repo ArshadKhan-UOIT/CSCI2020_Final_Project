12c2c6a89572bbc572e3dbf056fd39b19004a9c0
ArshadKhan-UOIT <arshad.khan@ontariotechu.net>
2020-03-26 20:10:44 -0400

    updating master/comments

--- src/main/java/CourseContent/Pages/GradesPage.java
+++ src/main/java/CourseContent/Pages/GradesPage.java
@@ -21,107 +21,91 @@ import java.util.List;
 
 public class GradesPage extends Page {
     //string.format("%.2f",numbers)
-    PieChart p1 = new PieChart(), p2 = new PieChart();
+    PieChart p1 = new PieChart(), p2 = new PieChart();  //declearing the pie chart stuff
     HBox hbox;
     VBox vbox;
-    Canvas canvas,canvas2;
-    GraphicsContext g,g2;
     String[] typesOfGrades = {"assignments","midterms","exams"};
     Integer[] sizeOfCSV = {5,7,7};
-    TableView<Row> grades;
-    TableColumn typeCol = new TableColumn("Type");
+    TableView<Row> grades;      //declearing the table view
+    TableColumn typeCol = new TableColumn("Type");     //these are the table columns for for the "grades" table view
     TableColumn totalCol = new TableColumn("Total");
     TableColumn gainedCol = new TableColumn("Gained");
     TableColumn neitherCol = new TableColumn("Neither");
     TableColumn lostCol = new TableColumn("Lost");
-    TableView<Row> totalTable;
-    TableColumn totTypeCol = new TableColumn("Type");
+    TableView<Row> totalTable;  //declearing the table view
+    TableColumn totTypeCol = new TableColumn("Type");   //these are the table columns for for the "totalTable" table view
     TableColumn totTotalCol = new TableColumn("Total");
     TableColumn totGainedCol = new TableColumn("Gained");
     TableColumn totNeitherCol = new TableColumn("Neither");
     TableColumn totLostCol = new TableColumn("Lost");
     Text courseMarks;
-//    Color[] pieColours = {Color.AQUA, Color.GOLD, Color.DARKORANGE,Color.DARKSALMON, Color.LAWNGREEN, Color.PLUM};
 
     public GradesPage() {
 
         hbox = new HBox();
         vbox = new VBox();
 
-        List<String[]> courses = CSVChanger.read("courses.csv",6);
-        makeButtons(courses);
+        List<String[]> courses = CSVChanger.read("courses.csv",6);  //reads the courses csv thats in CSV/CSVChanger.java
+        makeButtons(courses);   //makes the buttons at the top of the screen
         mainPane.add(hbox,0,1);
-//        for (int i=0;i< Window.courses.length;i++) {
-//            courseMarks = new Label(StringUtils.capitalize(courses.get(0)[0]) + " Current Grades:");
         courseMarks = new Text(StringUtils.capitalize(courses.get(0)[0]) + " Current Grades:");
-//        }
         courseMarks.setFont(new Font(30));
         mainPane.add(courseMarks,0,0);
         grades = new TableView<>();
         grades.setMaxHeight(125);
         grades.setMaxWidth(349);
-        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+        typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));    //setting everything for the "grades" table
         totalCol.setCellValueFactory(new PropertyValueFactory<>("total"));
         gainedCol.setCellValueFactory(new PropertyValueFactory<>("gained"));
         neitherCol.setCellValueFactory(new PropertyValueFactory<>("neither"));
         lostCol.setCellValueFactory(new PropertyValueFactory<>("lost"));
 
-        grades.getColumns().addAll(typeCol,gainedCol,lostCol,neitherCol,totalCol);
-//        canvas = new Canvas(200,200);
-//        g = canvas.getGraphicsContext2D();
-        makeTable(courses.get(0)[2]);
-//        mainPane.add(canvas,2,2);
-        mainPane.add(grades,0,2);
+        grades.getColumns().addAll(typeCol,gainedCol,lostCol,neitherCol,totalCol);  //adding in all of the columns
+        makeTable(courses.get(0)[2]);   //calls this function which initializes the table columns and pie chart based on the type of course
+        mainPane.add(grades,0,2);   //adding it to the pane
 
         totalTable = new TableView<>();
         totalTable.setMaxHeight(125);
         totalTable.setMaxWidth(352);
-        totTypeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
+        totTypeCol.setCellValueFactory(new PropertyValueFactory<>("type"));     //setting everything for the "total table" table
         totTotalCol.setCellValueFactory(new PropertyValueFactory<>("total"));
         totGainedCol.setCellValueFactory(new PropertyValueFactory<>("gained"));
         totNeitherCol.setCellValueFactory(new PropertyValueFactory<>("neither"));
         totLostCol.setCellValueFactory(new PropertyValueFactory<>("lost"));
-        totalTable.getColumns().addAll(totTypeCol,totGainedCol,totLostCol,totNeitherCol,totTotalCol);
-//        canvas2 = new Canvas(200,200);
-//        g2 = canvas2.getGraphicsContext2D();
-        makeTotalTable();
+        totalTable.getColumns().addAll(totTypeCol,totGainedCol,totLostCol,totNeitherCol,totTotalCol);   //adding in all of the columns
+        makeTotalTable();   //calls this function which initializes the table columns and pie chart based on the over total
         mainPane.add(p1,2,2);
         mainPane.add(p2,2,3);
-//        mainPane.add(canvas2,2,3);
         mainPane.add(totalTable,0,3);
     }
 
-    public void makeButtons(List<String[]> courses){
-        for (int x=0; x<courses.size();x++){
+    public void makeButtons(List<String[]> courses){    //makes the buttons
+        for (int x=0; x<courses.size();x++){    //initializes all the stuff for a button
             Button button = new Button();
-
             button.setText(courses.get(x)[0]);
             String cc = courses.get(x)[2];
             final int index = x;
-            button.setOnAction(e->{
-                makeTable(cc);
-                getLabel(index);
+            button.setOnAction(e->{ //when a button is clicked it ...
+                makeTable(cc);  //makes the table
+                getText(index);    //rests the text
             });
-            hbox.getChildren().add(button);
+            hbox.getChildren().add(button); //adds them to an hbox so they are beside each other
         }
-
     }
 
-    private void getLabel(int index) {
+    private void getText(int index) {  //changes the text at the top based on with button is pressed
         List<String[]> courses = CSVChanger.read("courses.csv",6);
         courseMarks.setText(StringUtils.capitalize(courses.get(index)[0]) + " Current Grades:");
         courseMarks.setFont(new Font(30));
-//        mainPane.add(courseMarks,0,0);
     }
 
-    public void makeTable(String courseName){
+    public void makeTable(String courseName){       //initializes the grades table view, and gets the pie chart
         grades.getItems().clear();
         ObservableList<Row> data = FXCollections.observableArrayList();
         double subjectTotal = 0;
         double subjectNeither = 0;
         double subjectGained = 0;
         double subjectLost = 0;
-
         for (int x=0; x<typesOfGrades.length;x++){
             List<String[]> gradesList = CSVChanger.read(typesOfGrades[x]+".csv",sizeOfCSV[x]);
             String type = StringUtils.capitalize(typesOfGrades[x]);
@@ -148,18 +132,17 @@ public class GradesPage extends Page {
             }
             data.add(new Row(type,total,neither,gained,lost));
         }
-        makeGraph(subjectTotal,subjectNeither,subjectGained,subjectLost);
-        data.add(new Row("Total",subjectTotal,subjectNeither,subjectGained,subjectLost));
-        grades.setItems(data);
+        makeGraph(subjectTotal,subjectNeither,subjectGained,subjectLost);   //calls for pie chart function for each course
+        data.add(new Row("Total",subjectTotal,subjectNeither,subjectGained,subjectLost));       //this sets all of the data for the grades table view
+        grades.setItems(data);  //this sets it
     }
 
-    public void makeTotalTable(){
+    public void makeTotalTable(){   //initializes the totalTable view and pie chart (calls the total pie chart function)
         ObservableList<Row> total = FXCollections.observableArrayList();
         double studentTotal2 = 0;
         double studentNeither2 = 0;
         double studentGained2 = 0;
         double studentLost2 = 0;
-
         for (int x=0; x<typesOfGrades.length;x++){
             List<String[]> gradesList = CSVChanger.read(typesOfGrades[x]+".csv",sizeOfCSV[x]);
             String type = StringUtils.capitalize(typesOfGrades[x]);
@@ -167,7 +150,6 @@ public class GradesPage extends Page {
             double studentNeither = 0;
             double studentGained = 0;
             double studentLost = 0;
-
             for (int y=0; y<gradesList.size(); y++){
                     if (gradesList.get(y)[sizeOfCSV[x]-1].equalsIgnoreCase("N/A")){
                         studentNeither += Double.parseDouble(gradesList.get(y)[sizeOfCSV[x]-2]);
@@ -189,11 +171,8 @@ public class GradesPage extends Page {
         total.add(new Row("Total",studentTotal2,studentNeither2,studentGained2,studentLost2));
         totalTable.setItems(total);
     }
-
-    public void makeTotalGraph(double studentTotal,double  studentNeither, double studentGained,double studentLost){
-//        canvas2.getGraphicsContext2D().clearRect(0,0,canvas2.getWidth(),canvas2.getHeight());
-        double startAngle=0;
-        double endAngle=0;
+    // this is the pie chart that grafically displays basically the same info like the total table column
+    public void makeTotalGraph(double studentTotal,double  studentNeither, double studentGained,double studentLost){ ;
         double[] gradesArray = {studentNeither,studentGained,studentLost};
         String[] legend = {"Neither","Gained","Lost"};
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
@@ -202,23 +181,9 @@ public class GradesPage extends Page {
         }
         p2.setData(pieChartData);
         p2.setTitle("Overall");
-//        System.out.println(gradesArray[1]);
-//
-//        for(int x=0; x < typesOfGrades.length; x++) {
-//            g2.setFill(pieColours[x]);
-//            endAngle = ((gradesArray[x]/studentTotal)*360);
-//            g2.fillArc(0,0,200,200,startAngle,endAngle, ArcType.ROUND);
-//            startAngle += endAngle;
-//        }
-//
-//        if (Double.isNaN(endAngle)){
-//            g2.setFont(new Font(30));
-//            g2.setTextAlign(TextAlignment.CENTER);
-//            g2.fillText("Nothing to Grade",250,250);
-//        }
     }
 
-    public static class Row{
+    public static class Row {    //this helps the table views get their columns by using the observal list
 
         private final SimpleStringProperty type;
         private final SimpleStringProperty total;
@@ -257,10 +222,7 @@ public class GradesPage extends Page {
         }
     }
 
-    public void makeGraph(double total, double neither, double gained, double lost){
-//        canvas.getGraphicsContext2D().clearRect(0,0,canvas.getWidth(),canvas.getHeight());
-        double startAngle=0;
-        double endAngle=0;
+    public void makeGraph(double total, double neither, double gained, double lost){    //makes the pie chart that basically displays the same data as the "grades" table
         double[] gradesArray = {neither,gained,lost};
         String[] legend = {"Neither","Gained","Lost"};
         ObservableList<PieChart.Data> pieChartData = FXCollections.observableArrayList();
@@ -270,19 +232,5 @@ public class GradesPage extends Page {
         p1.setData(pieChartData);
         p1.setTitle("Subject");
         p1.setClockwise(false);
-//        System.out.println(gradesArray[1]);
-
-//        for(int x=0; x < typesOfGrades.length; x++) {
-//            g.setFill(pieColours[x]);
-//            endAngle = ((gradesArray[x]/total)*360);
-//            g.fillArc(0,0,200,200,startAngle,endAngle, ArcType.ROUND);
-//            startAngle += endAngle;
-//        }
-//
-//        if (Double.isNaN(endAngle)){
-//            g.setFont(new Font(30));
-//            g.setTextAlign(TextAlignment.CENTER);
-//            g.fillText("Nothing to Grade",250,250);
-//        }
     }
 }
